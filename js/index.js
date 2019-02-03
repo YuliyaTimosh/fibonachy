3888b00c533072335e936f05975f8b1e3bb31af5
Yuliia <yuliyatimosh96@ukr.net>
2019-02-03 18:53:57 +0000

    reated varian 2

--- js/index.js
+++ js/index.js
@@ -1,20 +1,28 @@
-let usernumber = parseFloat(prompt('Enter the number'));
+//variant 1:
+
+const userNumber = parseFloat(prompt('Enter the number'));
 
 let fib = [];
 fib[0] = 1;
 fib[1] = 1;
 
-for (let i = 2; i <usernumber; i++) {
-    
-    fib[i] = fib[i - 2] + fib[i - 1];  
-    
-    }
- 
- alert(fib[usernumber - 1]);
- 
- 
+for (let i = 2; i < userNumber; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+}
 
 
+alert(fib[userNumber - 1]);
 
+//variant 2:
 
+let previous = 1;
+let beforePrevious = 1;
+let resultFib;
 
+for (let i = 2; i < userNumber; i++) {
+    resultFib = previous + beforePrevious;
+    beforePrevious = previous;
+    previous = resultFib;
+}
+
+console.log('result=' + resultFib);
